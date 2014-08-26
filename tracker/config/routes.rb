8e78cc0bc32a6dc2cf6abeacf8a45c5fa0d5c95c
bemyhre <ben.myhre35@gmail.com>
2014-08-26 10:08:34 -0500

    added model for client, projects, deliverables and issues

--- tracker/config/routes.rb
+++ tracker/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :deliverables
   resources :issues
 
+  root 'clients#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
